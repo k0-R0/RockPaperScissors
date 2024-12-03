8a3ff6f272a3705ab5cfcde231948e91900bb80a
k0r0 <88573975+k0-R0@users.noreply.github.com>
2024-12-03 19:23:35 +0530

    rock paper scissors completed

--- game.js
+++ game.js
@@ -1,1 +1,40 @@
-console.log("test");
+function getComputerChoice(){
+    randomNumber = Math.floor(Math.random()*(3));
+    const choice = ["Rock","Paper","Scissors"];
+    return choice[randomNumber];
+}
+
+function getHumanChoice(){
+    return prompt("pick a value from the 3: Rock, Paper, Scissors : ");
+}
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(){
+    let computerChoice = getComputerChoice().toLowerCase();
+    let humanChoice = getHumanChoice().toLowerCase();
+    if(computerChoice===humanChoice)
+        return;
+    else if(computerChoice==="rock"){
+        humanScore = (humanChoice==="paper")?10:0;
+        computerScore = (humanChoice==="paper")?0:10;
+    }
+    else if(computerChoice==="paper"){
+        humanScore = (humanChoice==="scissors")?10:0;
+        computerScore = (humanChoice==="scissors")?0:10;
+    }
+    else {
+        humanScore = (humanChoice==="rock")?10:0;
+        computerScore = (humanChoice==="rock")?0:10;
+    }
+    console.log((humanScore>computerScore)?
+    `human won - ${humanChoice} beats ${computerChoice}`:
+    `computer won - ${computerChoice} beats ${humanChoice}`);
+    console.log(`human: ${humanScore} computer: ${computerScore}`);
+}
+function playGame(){
+    for(i=0;i<5;i++)
+        playRound();
+    console.log((humanScore>computerScore)?`human won`:`computer won`);
+}
+playGame();
